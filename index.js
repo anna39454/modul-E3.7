ff746a01ff29aad004380c3af1f8c4b666b2dbba
anna39454 <154275998+anna39454@users.noreply.github.com>
2024-11-25 22:28:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+/*let myAge=25;
+console.log('я Анна и мой возраст ' + myAge);
+
+  комментарий 
+  комментарий
+
+usd = prompt('введите колличество долларов');
+rub = usd * 100.57;
+alert(usd + ' долларов равно ' + rub + 'рублей') //комментарий  */
+
+x = prompt('введите x');
+n = prompt('введите n');
+
+
+//let x = 3;
+//let n = 2;
+const f = (x, n) =>   x ** n
+  
+ console.log(f(x,n));
